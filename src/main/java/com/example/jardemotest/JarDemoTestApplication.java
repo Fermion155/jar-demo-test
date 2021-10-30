16b1f60ba80e06b67a2518989667c559bab7b828
Paulius Drabuzinskis <pdrabuzinskis@gmail.com>
2021-10-30 16:57:24 +0300

    first commit

---
+++ src/main/java/com/example/jardemotest/JarDemoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jardemotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarDemoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarDemoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jardemotest/JarDemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jardemotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarDemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
